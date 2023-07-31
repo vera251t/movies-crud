f38cb7ce4e6d37d54bbbbcd30e9b5740436f243d
vera251t <jhonnyvera0@gmail.com>
2023-07-31 16:27:39 -0500

    first commit

---
+++ src/routes/movie.router.js
@@ -0,0 +1,24 @@
+const { getAll, create, getOne, remove, update, setGenres, setActors, setDirectors } = require('../controllers/movie.controller');
+const express = require('express');
+
+const routerMovie = express.Router();
+
+routerMovie.route('/')
+    .get(getAll)
+    .post(create);
+
+routerMovie.route('/:id')
+    .get(getOne)
+    .delete(remove)
+    .put(update);
+
+routerMovie.route('/:id/genres')
+    .post(setGenres)
+
+routerMovie.route('/:id/actors')
+    .post(setActors)
+
+routerMovie.route('/:id/directors')
+    .post(setDirectors)
+    
+module.exports = routerMovie;
